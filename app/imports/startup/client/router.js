3a5cd827b5378bb5a67daa1a2f868323b1a2e586
Brent Nunogawa <nunogawa@hawaii.edu>
2016-10-27 22:28:29 -1000

    commit

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -8,6 +8,41 @@ FlowRouter.route('/', {
   },
 });
 
+FlowRouter.route('/create-club', {
+  name: 'Create_Club_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Create_Club_Page' });
+  },
+});
+
+FlowRouter.route('/create-event', {
+  name: 'Create_Event_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Create_Event_Page' });
+  },
+});
+
+FlowRouter.route('/club-stat', {
+  name: 'Club_Stat_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Club_Stat_Page' });
+  },
+});
+
+FlowRouter.route('/profile', {
+  name: 'Profile_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Profile_Page' });
+  },
+});
+
+FlowRouter.route('/find-club', {
+  name: 'Find_Club_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Find_Club_Page' });
+  },
+});
+
 FlowRouter.route('/list', {
   name: 'List_Stuff_Page',
   action() {
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,6 +1,21 @@
 import './add-stuff-page.html';
 import './add-stuff-page.js';
 
+import './create-club-page.html';
+import './create-club-page.js';
+
+import './create-event-page.html';
+import './create-event-page.js';
+
+import './profile-page.html';
+import './profile-page.js';
+
+import './club-stat-page.html';
+import './club-stat-page.js';
+
+import './find-club.html';
+import './find-club.js';
+
 import './edit-stuff-page.html';
 import './edit-stuff-page.js';
 
